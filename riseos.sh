0cf08c26a757ffedc176ba0919416163431fcf24
Thelupin07 <www.raj.coe@gmail.com>
2024-10-19 02:00:53 +0530

    Update built.sh

--- riseos.sh
+++ riseos.sh
@@ -4,52 +4,21 @@
 rm -rf .repo/local_manifests/
 
 # Initialize the ROM manifest using RisingTechOSS repository (branch 'fourteen')
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+mkdir ten
+
+repo init -u https://github.com/TenX-OS/manifest.git -b fourteen --git-lfs
 
 # Synchronize the repository using the custom 'resync.sh' script
-/opt/crave/resync.sh
+repo sync
 
-# Clean up unnecessary directories after repo sync
-rm -rf hardware/qcom-caf/sm8150/media   # Remove outdated Qualcomm CAF media hardware
-rm -rf vendor/lineage                   # Remove existing LineageOS vendor files
+git clone https://github.com/PixysOS-Devices/device_nothing_Spacewar.git device/nothing/Spacewar
 
-# Clone the device tree repositories for Xiaomi Sunny and related kernel, common configurations
-# Device-specific tree
-git clone https://github.com/dpenra-sunny/device_xiaomi_sunny.git --depth 1 -b fourteen device/xiaomi/sunny
-git clone https://github.com/dpenra-sunny/device_xiaomi_sunny-kernel.git --depth 1 -b fourteen device/xiaomi/sunny-kernel
+git clone https://github.com/PixysOS-Devices/vendor_nothing_Spacewar vendor/nothing/Spacewar
 
-# Clone Qualcomm common device tree (QSSI) and shared Qualcomm configurations
-git clone https://github.com/AOSPA/android_device_qcom_qssi.git --depth 1 -b uvite device/qcom/qssi
-git clone https://github.com/yaap/device_qcom_common.git --depth 1 -b fourteen device/qcom/common
+git clone https://github.com/BrainKub/android_kernel_nothing_sm7325 kernel/nothing/sm7325
 
-# (Optional) Clone the kernel tree for Xiaomi Sunny (commented out for now)
-# git clone https://github.com/PixelOS-Devices/kernel_xiaomi_sunny.git --depth 1 -b fourteen kernel/xiaomi/sunny
+git clone https://github.com/PixysOS-Devices/packages_apps_PixysGlyph packages/apps/PixysGlyph
 
-# Clone the vendor repositories for Xiaomi Sunny and Qualcomm components
-git clone https://github.com/PixelOS-Devices/vendor_xiaomi_sunny.git --depth 1 -b fourteen vendor/xiaomi/sunny
-git clone https://gitlab.com/yaosp/vendor_qcom_common.git --depth 1 -b fourteen vendor/qcom/common
-git clone https://github.com/yaap/vendor_qcom_opensource_core-utils.git --depth 1 -b fourteen vendor/qcom/opensource/core-utils
+git clone https://gitlab.com/PixysOS/vendor_nothing_camera -b fourteen vendor/nothing/camera
 
-# Clone hardware-specific files for Xiaomi devices
-git clone https://github.com/PixelOS-AOSP/hardware_xiaomi.git --depth 1 -b fourteen hardware/xiaomi
-
-# Clone prebuilt GCC compilers (used for building the kernel and other components)
-git clone https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf.git --depth 1 -b 14.0.0 prebuilts/gcc/linux-x86/aarch64/aarch64-elf
-git clone https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_arm_arm-eabi.git --depth 1 -b 12.0.0 prebuilts/gcc/linux-x86/arm/arm-eabi
-
-# Clone packages such as DisplayFeatures and KProfiles for extended functionality
-git clone https://github.com/cyberknight777/android_packages_apps_DisplayFeatures.git --depth 1 -b master packages/apps/DisplayFeatures
-git clone https://github.com/KProfiles/android_packages_apps_Kprofiles.git --depth 1 -b main packages/apps/KProfiles
-
-# Clone source modifications, including media hardware and vendor Lineage files
-git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media.git --depth 1 -b fourteen hardware/qcom-caf/sm8150/media
-git clone https://github.com/dpenra-sunny/android_vendor_lineage.git --depth 1 -b fourteen vendor/lineage
-
-# Set up the build environment (source environment setup script)
-. build/envsetup.sh
-
-# Select build target (lunch) for Xiaomi Sunny device with user build type
-riseup sunny user
-
-# Start the build process using 'rise' command (equivalent to 'make')
-rise b
+git clone https://gitlab.com/PixysOS-fourteen/vendor_nothing_Spacewar_radio vendor/nothing/Spacewar
